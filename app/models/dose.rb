8a0e8a430b9d9dda5dc388e434b7ff9ad7f5ee9d
John MacAulay <merjohn@bigpond.net.au>
2021-02-25 14:55:05 +1100

    seed done

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,7 +3,6 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence:true
-  
-
+  validates_uniqueness_of :cocktail_id, scope: [:ingredient_id]
  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+# doc = Nokogiri::HTML(URI.open('https://www.taste.com.au/recipes/collections?q=chocolate'))
+# titles = []
+# doc.search('.col-sm-7').each do |element|
+#     titles << element.search('.title').text.strip
+# end
+# p titles
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+user["drinks"].each {|ingred| next_ingred= Ingredient.create(name:ingred["strIngredient1"])  }
