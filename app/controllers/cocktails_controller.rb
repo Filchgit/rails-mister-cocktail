24d317805d655d55d47b387a83436cac5579e522
John MacAulay <merjohn@bigpond.net.au>
2021-02-25 23:19:42 +1100

    think I have the link to dose new page correct

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,5 @@
 class CocktailsController < ApplicationController
+  before_action :set_restaurant, only: %i[ show ]
   def index
   end
 
@@ -6,5 +7,30 @@ class CocktailsController < ApplicationController
   end
 
   def new
+    @cocktail = Cocktail.new
   end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+       redirect_to new_cocktail_dose_path(@cocktail.id) 
+    else
+      render cocktails_new_path 
+    end
+
+  end
+
+
+  
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_cocktail
+      @cocktail = Cocktail.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def cocktail_params
+      params.require(:cocktail).permit(:name)
+    end
+
 end
